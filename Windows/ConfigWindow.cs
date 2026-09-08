95fefb43a0baa12107f01c4f0e0e2c04c57cd518
ScareCrowLegion <147884875+ScareCrowLegion@users.noreply.github.com>
2026-09-07 23:47:07 -0400

    Redesign config window navigation and help text.

--- Windows/ConfigWindow.cs
+++ Windows/ConfigWindow.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Numerics;
 using Dalamud.Bindings.ImGui;
 using Dalamud.Interface.Windowing;
@@ -7,8 +6,21 @@ namespace QuestTextRecolor.Windows;
 
 public class ConfigWindow : Window
 {
+    private enum ConfigPage
+    {
+        General,
+        Colors,
+        TextStyle,
+        PopupLayout,
+        Presets,
+        Textures,
+        About
+    }
+
     private readonly Plugin plugin;
     private readonly Configuration configuration;
+
+    private ConfigPage currentPage = ConfigPage.General;
 
     public ConfigWindow(Plugin plugin)
         : base("Quest Text Recolor")
@@ -19,43 +31,140 @@ public class ConfigWindow : Window
         Flags = ImGuiWindowFlags.NoResize |
                 ImGuiWindowFlags.NoCollapse;
 
-        Size = new Vector2(500, 520);
+        Size = new Vector2(680, 520);
         SizeCondition = ImGuiCond.Always;
     }
 
     public override void Draw()
     {
+        ImGui.BeginChild(
+            "Navigation",
+            new Vector2(150f, 0f),
+            true
+        );
+
+        DrawNavigation();
+
+        ImGui.EndChild();
+
+        ImGui.SameLine();
+
+        ImGui.BeginChild(
+            "Content",
+            new Vector2(0f, 0f),
+            false
+        );
+
+        DrawCurrentPage();
+
+        ImGui.EndChild();
+    }
+
+    private void DrawNavigation()
+    {
+        ImGui.Text("Settings");
+        ImGui.Separator();
+        ImGui.Spacing();
+
+        if (ImGui.Selectable(
+            "General",
+            currentPage == ConfigPage.General))
+        {
+            currentPage = ConfigPage.General;
+        }
+
+        ImGui.Spacing();
+
+        if (ImGui.Selectable(
+            "Colors",
+            currentPage == ConfigPage.Colors))
+        {
+            currentPage = ConfigPage.Colors;
+        }
+
+        ImGui.Spacing();
+
+        if (ImGui.Selectable(
+            "Text Style",
+            currentPage == ConfigPage.TextStyle))
+        {
+            currentPage = ConfigPage.TextStyle;
+        }
+
+        ImGui.Spacing();
+
+        if (ImGui.Selectable(
+            "Popup Layout",
+            currentPage == ConfigPage.PopupLayout))
+        {
+            currentPage = ConfigPage.PopupLayout;
+        }
+
+        ImGui.Spacing();
+
+        if (ImGui.Selectable(
+            "Presets",
+            currentPage == ConfigPage.Presets))
+        {
+            currentPage = ConfigPage.Presets;
+        }
+
+        ImGui.Spacing();
+
+        if (ImGui.Selectable(
+            "Textures",
+            currentPage == ConfigPage.Textures))
+        {
+            currentPage = ConfigPage.Textures;
+        }
+
+        ImGui.Spacing();
+
+        if (ImGui.Selectable(
+            "About",
+            currentPage == ConfigPage.About))
+        {
+            currentPage = ConfigPage.About;
+        }
+    }
+
+    private void DrawCurrentPage()
+    {
+        switch (currentPage)
+        {
+            case ConfigPage.General:
+                DrawGeneralPage();
+                break;
+
+            case ConfigPage.Colors:
+                DrawColorSection();
+                break;
+
+            case ConfigPage.TextStyle:
+                DrawTextStyleSection();
+                break;
+
+            case ConfigPage.PopupLayout:
+                DrawPositionSection();
+                break;
+
+            case ConfigPage.Presets:
+                DrawPresetSection();
+                break;
+
+            case ConfigPage.Textures:
+                DrawTextureSection();
+                break;
+
+            case ConfigPage.About:
+                DrawAboutSection();
+                break;
+        }
+    }
+
+    private void DrawGeneralPage()
+    {
         DrawPluginSection();
-
-        ImGui.Spacing();
-        ImGui.Separator();
-        ImGui.Spacing();
-
-        DrawColorSection();
-
-        ImGui.Spacing();
-        ImGui.Separator();
-        ImGui.Spacing();
-
-        DrawTextStyleSection();
-
-        ImGui.Spacing();
-        ImGui.Separator();
-        ImGui.Spacing();
-
-        DrawPositionSection();
-
-        ImGui.Spacing();
-        ImGui.Separator();
-        ImGui.Spacing();
-
-        DrawPresetSection();
-
-        ImGui.Spacing();
-        ImGui.Separator();
-        ImGui.Spacing();
-
-        DrawTextureSection();
 
         ImGui.Spacing();
         ImGui.Separator();
@@ -76,14 +185,16 @@ public class ConfigWindow : Window
 
         var enabled = configuration.EnableQuestTextRecolor;
 
-        if (ImGui.Checkbox("Enable Quest Text Recolor", ref enabled))
+        if (ImGui.Checkbox(
+            "Enable Quest Text Recolor",
+            ref enabled))
         {
             configuration.EnableQuestTextRecolor = enabled;
             configuration.Save();
         }
 
         ImGui.TextDisabled(
-            "Recolors the center-screen quest objective progression text."
+            "Enable or disable quest popup text customization."
         );
     }
 
@@ -91,47 +202,79 @@ public class ConfigWindow : Window
     {
         ImGui.Text("Colors");
 
+        ImGui.TextDisabled(
+            "Customize the quest text and outline colors."
+        );
+
+        ImGui.Spacing();
+
         var textColor = configuration.QuestTextColor;
 
-        if (ImGui.ColorEdit4("Text Color", ref textColor))
+        if (ImGui.ColorEdit4(
+            "Text Color",
+            ref textColor))
         {
             configuration.QuestTextColor = textColor;
             configuration.Save();
         }
 
-        ImGui.TextDisabled("Plugin Default: #F2E4C4FF");
+        ImGui.TextDisabled(
+            "Plugin Default: #F2E4C4FF"
+        );
 
         ImGui.Spacing();
 
         var edgeColor = configuration.QuestEdgeColor;
 
-        if (ImGui.ColorEdit4("Edge Color", ref edgeColor))
+        if (ImGui.ColorEdit4(
+            "Edge Color",
+            ref edgeColor))
         {
             configuration.QuestEdgeColor = edgeColor;
             configuration.Save();
         }
 
-        ImGui.TextDisabled("Plugin Default: #5A4526FF");
+        ImGui.TextDisabled(
+            "Plugin Default: #5A4526FF"
+        );
     }
 
     private void DrawTextStyleSection()
     {
         ImGui.Text("Text Style");
 
+        ImGui.TextDisabled(
+            "Adjust the size of the quest objective text."
+        );
+
+        ImGui.Spacing();
+
         var fontSize = configuration.QuestFontSize;
 
-        if (ImGui.SliderInt("Font Size", ref fontSize, 12, 28))
+        if (ImGui.SliderInt(
+            "Font Size",
+            ref fontSize,
+            12,
+            28))
         {
             configuration.QuestFontSize = fontSize;
             configuration.Save();
         }
 
-        ImGui.TextDisabled("Plugin Default: 18");
+        ImGui.TextDisabled(
+            "Plugin Default: 18"
+        );
     }
 
     private void DrawPositionSection()
     {
-        ImGui.Text("Popup Position");
+        ImGui.Text("Popup Layout");
+
+        ImGui.TextDisabled(
+            "Adjust the position and size of the quest popup."
+        );
+
+        ImGui.Spacing();
 
         var offsetX = configuration.QuestPopupOffsetX;
 
@@ -159,7 +302,8 @@ public class ConfigWindow : Window
             configuration.Save();
         }
 
-        var scalePercent = configuration.QuestPopupScale * 100f;
+        var scalePercent =
+            configuration.QuestPopupScale * 100f;
 
         if (ImGui.SliderFloat(
             "Popup Scale",
@@ -168,13 +312,23 @@ public class ConfigWindow : Window
             150f,
             "%.0f%%"))
         {
-            configuration.QuestPopupScale = scalePercent / 100f;
+            configuration.QuestPopupScale =
+                scalePercent / 100f;
+
             configuration.Save();
         }
 
+        ImGui.Spacing();
+
         ImGui.TextDisabled(
-            "Offsets are measured in pixels from the default FFXIV popup layout."
+            "Offsets are relative to the default FFXIV popup position."
         );
+
+        ImGui.TextDisabled(
+            "100% restores the default FFXIV popup size."
+        );
+
+        ImGui.Spacing();
 
         if (ImGui.Button("Reset Layout"))
         {
@@ -189,107 +343,135 @@ public class ConfigWindow : Window
     {
         ImGui.Text("Presets");
 
-        if (ImGui.Button("Plugin Default", new Vector2(140, 0)))
-        {
-            configuration.QuestTextColor = new Vector4(
-                242f / 255f,
-                228f / 255f,
-                196f / 255f,
-                1.0f
-            );
+        ImGui.TextDisabled(
+            "Apply preset text and edge color combinations."
+        );
 
-            configuration.QuestEdgeColor = new Vector4(
-                90f / 255f,
-                69f / 255f,
-                38f / 255f,
-                1.0f
-            );
+        ImGui.Spacing();
+
+        if (ImGui.Button(
+            "Plugin Default",
+            new Vector2(140, 0)))
+        {
+            configuration.QuestTextColor =
+                new Vector4(
+                    242f / 255f,
+                    228f / 255f,
+                    196f / 255f,
+                    1.0f
+                );
+
+            configuration.QuestEdgeColor =
+                new Vector4(
+                    90f / 255f,
+                    69f / 255f,
+                    38f / 255f,
+                    1.0f
+                );
 
             configuration.Save();
         }
 
         ImGui.SameLine();
 
-        if (ImGui.Button("FFXIV Original", new Vector2(140, 0)))
+        if (ImGui.Button(
+            "FFXIV Original",
+            new Vector2(140, 0)))
         {
-            configuration.QuestTextColor = new Vector4(
-                1.0f,
-                1.0f,
-                1.0f,
-                1.0f
-            );
+            configuration.QuestTextColor =
+                new Vector4(
+                    1.0f,
+                    1.0f,
+                    1.0f,
+                    1.0f
+                );
 
-            configuration.QuestEdgeColor = new Vector4(
-                0f / 255f,
-                153f / 255f,
-                255f / 255f,
-                1.0f
-            );
+            configuration.QuestEdgeColor =
+                new Vector4(
+                    0f / 255f,
+                    153f / 255f,
+                    255f / 255f,
+                    1.0f
+                );
 
             configuration.Save();
         }
 
-        if (ImGui.Button("FFXIV Gold", new Vector2(140, 0)))
+        if (ImGui.Button(
+            "FFXIV Gold",
+            new Vector2(140, 0)))
         {
-            configuration.QuestTextColor = new Vector4(
-                232f / 255f,
-                196f / 255f,
-                110f / 255f,
-                1.0f
-            );
+            configuration.QuestTextColor =
+                new Vector4(
+                    232f / 255f,
+                    196f / 255f,
+                    110f / 255f,
+                    1.0f
+                );
 
-            configuration.QuestEdgeColor = new Vector4(
-                76f / 255f,
-                49f / 255f,
-                20f / 255f,
-                1.0f
-            );
+            configuration.QuestEdgeColor =
+                new Vector4(
+                    76f / 255f,
+                    49f / 255f,
+                    20f / 255f,
+                    1.0f
+                );
 
             configuration.Save();
         }
 
         ImGui.SameLine();
 
-        if (ImGui.Button("High Contrast", new Vector2(140, 0)))
+        if (ImGui.Button(
+            "High Contrast",
+            new Vector2(140, 0)))
         {
-            configuration.QuestTextColor = new Vector4(
-                245f / 255f,
-                245f / 255f,
-                245f / 255f,
-                1.0f
-            );
+            configuration.QuestTextColor =
+                new Vector4(
+                    245f / 255f,
+                    245f / 255f,
+                    245f / 255f,
+                    1.0f
+                );
 
-            configuration.QuestEdgeColor = new Vector4(
-                20f / 255f,
-                20f / 255f,
-                20f / 255f,
-                1.0f
-            );
+            configuration.QuestEdgeColor =
+                new Vector4(
+                    20f / 255f,
+                    20f / 255f,
+                    20f / 255f,
+                    1.0f
+                );
 
             configuration.Save();
         }
 
-        if (ImGui.Button("Cool Blue", new Vector2(140, 0)))
+        if (ImGui.Button(
+            "Cool Blue",
+            new Vector2(140, 0)))
         {
-            configuration.QuestTextColor = new Vector4(
-                190f / 255f,
-                220f / 255f,
-                255f / 255f,
-                1.0f
-            );
+            configuration.QuestTextColor =
+                new Vector4(
+                    190f / 255f,
+                    220f / 255f,
+                    255f / 255f,
+                    1.0f
+                );
 
-            configuration.QuestEdgeColor = new Vector4(
-                25f / 255f,
-                55f / 255f,
-                95f / 255f,
-                1.0f
-            );
+            configuration.QuestEdgeColor =
+                new Vector4(
+                    25f / 255f,
+                    55f / 255f,
+                    95f / 255f,
+                    1.0f
+                );
 
             configuration.Save();
         }
+
+        ImGui.Spacing();
 
         ImGui.TextDisabled(
-            "Presets update both text and edge colors."
+            "Presets change text and edge colors only."
         );
     }
 
@@ -297,59 +479,88 @@ public class ConfigWindow : Window
     {
         ImGui.Text("Quest Popup Textures");
 
+        ImGui.TextDisabled(
+            "Optionally replace the quest popup textures through Penumbra."
+        );
+
         ImGui.Spacing();
 
-        var penumbraAvailable = plugin.Penumbra.IsAvailable();
+        var penumbraAvailable =
+            plugin.Penumbra.IsAvailable();
 
         if (penumbraAvailable)
         {
-            ImGui.Text("Penumbra: Available");
-            ImGui.TextDisabled("Custom quest popup textures can be appiled through Penumbra.");
+            ImGui.Text(
+                "Penumbra: Available"
+            );
+
+            ImGui.TextDisabled(
+                "Custom quest popup textures can be applied."
+            );
         }
         else
         {
-            ImGui.TextDisabled("Penumbra: Not Available");
-            ImGui.TextDisabled("Install and enable Penumbra to use custom quest popup textures.");
+            ImGui.TextDisabled(
+                "Penumbra: Not Available"
+            );
+
+            ImGui.TextDisabled(
+                "Install and enable Penumbra to use custom quest popup textures."
+            );
         }
 
         ImGui.Spacing();
 
-        var enabled = configuration.EnableQuestPopupTextures;
+        var enabled =
+            configuration.EnableQuestPopupTextures;
 
-        ImGui.BeginDisabled(!penumbraAvailable);
+        ImGui.BeginDisabled(
+            !penumbraAvailable
+        );
 
         if (ImGui.Checkbox(
             "Enable Custom Quest Popup Textures",
             ref enabled))
         {
-            configuration.EnableQuestPopupTextures = enabled;
+            configuration.EnableQuestPopupTextures =
+                enabled;
+
             configuration.Save();
 
             if (enabled)
             {
-                plugin.Penumbra.ApplyQuestPopupTextures();
+                plugin.Penumbra
+                    .ApplyQuestPopupTextures();
             }
             else
             {
-                plugin.Penumbra.RemoveQuestPopupTextures();
+                plugin.Penumbra
+                    .RemoveQuestPopupTextures();
             }
         }
 
         ImGui.EndDisabled();
 
-        ImGui.TextDisabled("Penumbra is only required for the optional texture replacement feature.");
-        ImGui.TextDisabled("A full FFXIV restart is recommmended after enabling or disabling textures.");
+        ImGui.Spacing();
+
+        ImGui.TextDisabled(
+            "Penumbra is only required for this optional texture feature."
+        );
+
+        ImGui.TextDisabled(
+            "A full FFXIV restart is recommended after enabling or disabling textures."
+        );
     }
-
-
 
     private void DrawTestSection()
     {
         ImGui.Text("In-Game Preview");
 
         ImGui.TextDisabled(
-            "Preview your current colors and font size on screen."
+            "Preview your current text colors and font size in game."
         );
+
+        ImGui.Spacing();
 
         if (ImGui.Button("Show Test Popup"))
         {
@@ -358,20 +569,67 @@ public class ConfigWindow : Window
 
         ImGui.SameLine();
 
-        ImGui.TextDisabled("Displays for 4 seconds.");
+        ImGui.TextDisabled(
+            "Displays for 4 seconds."
+        );
     }
 
     private void DrawResetSection()
     {
-        if (ImGui.Button("Reset Appearance to Plugin Default"))
+        ImGui.Text("Appearance Reset");
+
+        ImGui.TextDisabled(
+            "Restore the plugin's default text appearance."
+        );
+
+        ImGui.Spacing();
+
+        if (ImGui.Button(
+            "Reset Appearance to Plugin Default"))
         {
             configuration.ResetAppearance();
         }
 
-        ImGui.SameLine();
+        ImGui.Spacing();
 
         ImGui.TextDisabled(
-            "Restores plugin default colors and font size."
+            "Reset text color, edge color, and font size."
+        );
+    }
+
+    private void DrawAboutSection()
+    {
+        ImGui.Text("Quest Text Recolor");
+
+        ImGui.Spacing();
+
+        ImGui.TextWrapped(
+            "Customize FFXIV's center-screen quest objective progression text and popup layout."
+        );
+
+        ImGui.Spacing();
+        ImGui.Separator();
+        ImGui.Spacing();
+
+        ImGui.Text("Command");
+        ImGui.TextDisabled("/questtext");
+
+        ImGui.Spacing();
+
+        ImGui.Text("Features");
+        ImGui.TextDisabled(
+            "Text colors, font size, popup position, scale, presets, and optional textures."
+        );
+
+        ImGui.Spacing();
+
+        ImGui.Text("Penumbra");
+        ImGui.TextDisabled(
+            "Only required for the optional custom quest popup textures."
+        );
+
+        ImGui.TextDisabled(
+            "Additional About information will be added during the v1.3.0 polish pass."
         );
     }
 }
